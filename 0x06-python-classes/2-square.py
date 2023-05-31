964d7543f047e48d0f9496e0640ea09482ca1cb4
manchor127 <manchor127@gmail.com>
2023-05-31 11:51:48 +0000

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""defineass Square"""
+"""defines a class Square"""
 
 
 class Square:
     """Rts a square
 
     Attributes:
-        __size size of a side of the square
+        __size (int) side of the square
     """
     def __init__(self, size=0):
-        """initia
+        """initializes the square
+
         Args:
-            size (int):  a side of the square
+            size (int)ide of the square
 
         Returns:
             None
         """
         if type(size) is not int:
-            raise TypeError("size e an integer")
+            raise TypeError("size must be an integer")
         else:
             if size < 0:
-                raise ValueError("size m be >= 0")
+                raise ValueError("size must be >= 0")
             else:
                 self.__size = size
